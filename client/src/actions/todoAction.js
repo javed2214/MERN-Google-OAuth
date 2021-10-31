b9c845e18e7414676be5088d35571de1a415ad06
Javed Ansari <javed.ansari4122@gmail.com>
2021-10-31 19:25:10 +0530

    Added Todo Action

---
+++ client/src/actions/todoAction.js
@@ -0,0 +1,48 @@
+import axios from 'axios'
+import { ADD_TODO, ADD_TODO_ERROR, GET_TODOS, GET_TODOS_ERROR } from '../constants'
+
+export const addTodo = (todo) => {
+    return (dispatch, getState) => {
+        axios.post('/todo/addtodo', { todo })
+            .then(() => {
+                console.log(resp.data)
+                if(resp.data.success == true){
+                    dispatch({
+                        type: ADD_TODO,
+                        payload: resp.data.todo
+                    })
+                } else{
+                    dispatch({
+                        type: ADD_TODO_ERROR,
+                        payload: null
+                    })
+                }
+            })
+            .catch((err) => {
+                console.log('Error in Adding Todos')
+            })
+    }
+}
+
+export const getTodos = () => {
+    return (dispatch) => {
+        axios.get('/todo/gettodos')
+            .then(() => {
+                console.log(resp.data)
+                if(resp.data.success == true){
+                    dispatch({
+                        type: GET_TODOS,
+                        payload: resp.data.todos
+                    })
+                } else{
+                    dispatch({
+                        type: GET_TODOS_ERROR,
+                        payload: null
+                    })
+                }
+            })
+            .catch((err) => {
+                console.log('Error in getting Todos')
+            })
+    }
+}
